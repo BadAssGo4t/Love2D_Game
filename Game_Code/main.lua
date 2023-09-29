7b73526a370830b03f51807f26b38b32e97e69e2
Estanislao <estanislao.sb@gmail.com>
2023-09-29 13:01:27 -0300

    Frogger Start
    Strated the code so i can build a  frogger game
    Added Resource of Frog PNG

--- Game_Code/main.lua
+++ Game_Code/main.lua
@@ -2,32 +2,63 @@ function love.load()
    
   love.window.setTitle("Goat_Game")
   
-  love.graphics.setBackgroundColor(1, 1, 1)
-  spaceshipImage = love.graphics.newImage("spaceship.png")
+  love.graphics.setBackgroundColor(0, 0, 0)
+  frogImage = love.graphics.newImage("frog.png")
   
+   lock_Up_Down=false
+   lock_Rigth_Left=false
   
-  posX = 0
-  posY = 0
+  frog = {
+    posX = 0,
+    posY = 0
+	}
+
+  
+  recX = 200
+  recY = 0
 
 
-  speed = 10.0
+  speed = 150.0
 
 end
 
+function love.keypressed(key, scancode, isrepeat)
+   if key == "escape" then
+      love.event.quit()
+   end
+end
 
 function love.update(dt)
+   
   
-  posX = posX + speed * dt
-  posY = posY + speed * dt
+       if recY < 620 then
+	    recY = recY + 300 * dt
+		end
+		if recY >= 620 then 
+		recY = 0
+        end
   
-  print(posX)
-  print(posY)
+  
+ if love.keyboard.isDown("a") then
+        frog.posX = frog.posX - speed * dt
+elseif love.keyboard.isDown("d") then
+        frog.posX = frog.posX + speed * dt
+elseif love.keyboard.isDown("w") then
+        frog.posY = frog.posY - speed * dt
+elseif love.keyboard.isDown("s") then
+        frog.posY = frog.posY + speed * dt
+end
+
+  print(frog.posX)
+  print(frog.posY)
   
 end
 
 
 function love.draw()
   
-  love.graphics.draw(spaceshipImage, posX, posY)
+  love.graphics.rectangle("line", recX, recY, 50, 50)
+  
+  love.graphics.draw( frogImage, frog.posX, frog.posY )
 
 end
